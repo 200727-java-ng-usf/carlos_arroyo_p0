c490ddd3e0649af6643a732ba291970453dce9fe
carlos arroyo <coarroyo30@gmail.com>
2020-08-25 10:54:02 -0400

    added account repository

--- src/main/java/com/revature/project0/repo/AccountRepository.java
+++ src/main/java/com/revature/project0/repo/AccountRepository.java
@@ -1,5 +1,12 @@
 package com.revature.project0.repo;
 
+import com.revature.project0.models.Account;
+import com.revature.project0.util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 
 public class AccountRepository {
@@ -8,5 +15,44 @@ public class AccountRepository {
         System.out.println("Instantiating " + this.getClass().getName());
     }
 
-    public Optional
+    public Optional<Account> findAccountById(double balance) {
+        Optional<Account> _user = Optional.empty();
+
+        try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
+
+            String sql = "SELECT account_balance FROM project_0.account a " +
+                         "WHERE account_id = ? ";
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setDouble(1, balance);
+
+
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+        return _user;
+    }
+
+    public void save(Account newBalance) {
+        try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
+
+            String sql = "INSERT INTO project_0.account(account_balance) " +
+                         "VALUES(?)";
+
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setDouble(1, newBalance.getBalance());
+
+            int dataInserted = pstmt.executeUpdate();
+
+            if (dataInserted != 0) {
+                ResultSet rs = pstmt.getGeneratedKeys();
+
+                rs.next();
+                newBalance.setBalance(rs.getDouble(1));
+            }
+
+        }catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/revature/project0/services/AccountServices.java
+++ src/main/java/com/revature/project0/services/AccountServices.java
@@ -2,7 +2,6 @@ package com.revature.project0.services;
 
 import com.revature.project0.repo.AccountRepository;
 
-import java.math.BigDecimal;
 
 import static com.revature.project0.AppDriver.app;
 
